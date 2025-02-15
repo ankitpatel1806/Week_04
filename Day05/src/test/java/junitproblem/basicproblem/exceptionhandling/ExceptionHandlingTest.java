e1e4252a24a328ca606ffe2c44bb40226dc4dc69
Ankit Patel <yugap1806@gmail.com>
2025-02-15 13:37:31 +0530

    Ankit Patel Added Testing Exception Handling

---
+++ Day05/src/test/java/junitproblem/basicproblem/exceptionhandling/ExceptionHandlingTest.java
@@ -0,0 +1,19 @@
+package junitproblem.basicproblem.exceptionhandling;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionHandlingTest {
+
+    ExceptionHandling exceptionHandling = new ExceptionHandling();
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> exceptionHandling.divide(10, 0));
+    }
+
+    @Test
+    public void testDivideNormal() {
+        assertEquals(5, exceptionHandling.divide(10, 2));
+    }
+}
