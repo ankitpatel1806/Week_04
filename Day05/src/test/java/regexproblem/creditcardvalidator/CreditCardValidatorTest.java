c0bb360899851ef3b1eb61babe9fd52e9f74e371
Ankit Patel <yugap1806@gmail.com>
2025-02-15 11:52:08 +0530

    Ankit Patel Added Validate a Credit Card Number (Visa, MasterCard, etc.)

---
+++ Day05/src/test/java/regexproblem/creditcardvalidator/CreditCardValidatorTest.java
@@ -0,0 +1,26 @@
+package regexproblem.creditcardvalidator;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class CreditCardValidatorTest {
+    @Test
+    public void testValidVisaCard() {
+        assertTrue(CreditCardValidator.isValidCreditCard("4111111111111111"));
+    }
+    @Test
+    public void testInvalidVisaCard() {
+        assertFalse(CreditCardValidator.isValidCreditCard("41111111111111"));
+    }
+    @Test
+    public void testValidMasterCard() {
+        assertTrue(CreditCardValidator.isValidCreditCard("5111111111111111"));
+    }
+    @Test
+    public void testInvalidMasterCard() {
+        assertFalse(CreditCardValidator.isValidCreditCard("51111111111111"));
+    }
+
+    @Test
+    public void testInvalidCardWithInvalidPrefix() {
+        assertFalse(CreditCardValidator.isValidCreditCard("6111111111111111"));
+    }
+}
