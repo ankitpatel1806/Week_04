bc1576a858ff613cb5a4fcbac84e93e666ea48ae
Ankit Patel <yugap1806@gmail.com>
2025-02-15 13:31:34 +0530

    Ankit Patel Added Testing String Utility Methods

---
+++ Day05/src/test/java/junitproblem/basicproblem/stringutils/StringUtilsTest.java
@@ -0,0 +1,28 @@
+package junitproblem.basicproblem.stringutils;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class StringUtilsTest {
+
+    StringUtils stringUtils = new StringUtils();
+
+    @Test
+    public void testReverse() {
+        assertEquals("olleh", stringUtils.reverse("hello"));
+    }
+
+    @Test
+    public void testIsPalindromeTrue() {
+        assertTrue(stringUtils.isPalindrome("madam"));
+    }
+
+    @Test
+    public void testIsPalindromeFalse() {
+        assertFalse(stringUtils.isPalindrome("hello"));
+    }
+
+    @Test
+    public void testToUpperCase() {
+        assertEquals("HELLO", stringUtils.toUpperCase("hello"));
+    }
+}
